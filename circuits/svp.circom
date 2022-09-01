34c1737083bc69e19b6d8f98f6ea455f2c6dd26d
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-09-01 11:58:22 +0200

    update testing

--- circuits/svp.circom
+++ circuits/svp.circom
@@ -1,5 +1,5 @@
 pragma circom 2.0.2;
 
-include "./lib/selected-verifier-proof2.circom";
+include "./lib/selected-verifier-proof.circom";
 
 component main  { public [root,addr]} = SelectedVerifierProof(64, 4, 32);
--- test/svp.js
+++ test/svp.js
@@ -18,101 +18,86 @@ function bigintToTuple(x) {
   return ret;
 }
 
-describe("Designated Verifier Testing", function() {
+describe("Designated Verifier Testing", function async() {
 
     this.timeout(1000 * 1000);
-    let circuit = await wasm_tester(path.join(__dirname, "../circuits", "svp.circom"));
 
     // Create a merkle tree and add leaves 
     const mt = new Group(32)
-    let leaves = ["12345", "7542394", "1234697"]
-    mt.addMembers(leaves)   
+    let leaves = [BigInt("12345"), BigInt("7542394"), BigInt("1234697")]
+    mt.addMembers(leaves)
 
     // Generate merkleProof for leaf 7542394
     let merkleProof = mt.generateProofOfMembership(mt.indexOf(leaves[1]))
 
+    // Generate a random privkey for the Verifier
+    const wallet = ethers.Wallet.createRandom()
+    let verifierPrivKey = wallet.privateKey
+    let verifierAddress = wallet.address
 
-    describe("Prover to Designated Verifier", function() {
+    describe("Prover to Designated Verifier", function () {
+    
 
-
-
-        // Prover generates a validProof
-
-        // invalidProof
-
-
+        it("should verify a valid proof (based on 1st condition) coming from the prover", async () => {
         
+            let circuit = await wasm_tester(path.join(__dirname, "../circuits", "svp.circom"));
+
+            // Prover generates a random privkey
+            const wallet2 = ethers.Wallet.createRandom()
+            let randPrvKey = wallet2.privateKey
+        
+            // Prover generates input that satisfies 1st condition and doesn't satisfy 2nd condition
+            let input = {
+                leaf: merkleProof.leaf,
+                pathIndices: merkleProof.pathIndices,
+                siblings: merkleProof.siblings,
+                root: merkleProof.root,
+                privkey: bigintToTuple(BigInt(randPrvKey)),
+                addr: verifierAddress,
+            }
+
+
+            const w = await circuit.calculateWitness(
+                input,
+                true
+            );
+
+            // Verifier should check that proof is valid and infer that this must be valid because it satisfies 1st condition 
+            await circuit.assertOut(w, {out: "1"})
+            await circuit.checkConstraints(w);
+        });
+
+        it("shouldn't verify an invalid proof (both conditions are not met) coming from the prover", async () => {
+        
+            let circuit = await wasm_tester(path.join(__dirname, "../circuits", "svp.circom"));
+
+            // Prover generates a random privkey that doesn't satisfy 1st condition (remember, the prover doesn't know the verifier's private key)
+            const wallet2 = ethers.Wallet.createRandom()
+            let randPrvKey = wallet2.privateKey
+
+            // generate a random leaf that is not part of the tree
+            let nonMemberLeaf = BigInt("6742389")
+        
+            // Prover generates input that doesn't satisfy both 1st and 2nd condition
+            let input = {
+                leaf: nonMemberLeaf,
+                pathIndices: merkleProof.pathIndices,
+                siblings: merkleProof.siblings,
+                root: merkleProof.root,
+                privkey: bigintToTuple(BigInt(randPrvKey)),
+                addr: verifierAddress,
+            }
+    
+            const w = await circuit.calculateWitness(
+                input,
+                true
+              );
+    
+            // Verifier should check that proof is not valid
+            await circuit.assertOut(w, {out: "0"})
+            await circuit.checkConstraints(w);
+        });
+
     });
 
-    describe("Designated Verifier to Third Party", function() {
-
-        
-        
-    });
-
-});
-
-
-
-
-
-  it("verify a valid proof coming from the prover to the designated verifier", async () => {
-    let circuit = await wasm_tester(path.join(__dirname, "../circuits", "svp.circom"));
-
-    validProof = "a"
-
-    // NOTE: same as circom-ecdsa test cases
-    let testPrivs = [
-      '0xc3c50b95a58172a4cce3e76629276a627a6d8626bcbbb7762e521207bbc59761',
-      '0x535d586f561155c728815b18c7a6836e4c8863d9a24a571818c49ad50a6a079c',
-      '0xc7d5ce7bee4ad0cd41c16aa685016b14ac5547575b84a574324c1fdd7eb54619',
-      '0xf55ac2f52c4b3712a7819ffdc4be600315e57767c081a8d43b2e41cdb0eda0cb'
-    ]
-
-    for (let priv of testPrivs) {
-      let address = ethers.utils.computeAddress(priv);
-      
-      const w = await circuit.calculateWitness(
-        {
-          privkey: bigintToTuple(BigInt(priv)),
-          addr: address
-        },
-        true
-      );
-      await circuit.assertOut(w, {out: "1"})
-      await circuit.checkConstraints(w);
-    }
-  });
-
-  it("verify that the same privkeys do not match random addresses", async () => {
-    let circuit = await wasm_tester(path.join(__dirname, "../circuits", "eth.circom"));
-
-    // NOTE: same as circom-ecdsa test cases
-    let testPrivs = [
-      '0xc3c50b95a58172a4cce3e76629276a627a6d8626bcbbb7762e521207bbc59761',
-      '0x535d586f561155c728815b18c7a6836e4c8863d9a24a571818c49ad50a6a079c',
-      '0xc7d5ce7bee4ad0cd41c16aa685016b14ac5547575b84a574324c1fdd7eb54619',
-      '0xf55ac2f52c4b3712a7819ffdc4be600315e57767c081a8d43b2e41cdb0eda0cb'
-    ]
-
-    let nonCorrelatedAddresses = [
-      '0xA85bd5bA932e4345df66b08C5794F3fD6F63cacC',
-      '0xA85bd5bA932e4345df66b08C5794F3fD6F63cacD',
-      '0xA85bd5bA932e4345df66b08C5794F3fD6F63cacE',
-      '0xA85bd5bA932e4345df66b08C5794F3fD6F63cacF'
-    ]
-
-    for (let i=0; i<testPrivs.length; i++) {
-      
-      const w = await circuit.calculateWitness(
-        {
-          privkey: bigintToTuple(BigInt(testPrivs[i])),
-          addr: BigInt(nonCorrelatedAddresses[i])
-        },
-        true
-      );
-      await circuit.assertOut(w, {out: "0"})
-      await circuit.checkConstraints(w);
-    }
-  });
 });
