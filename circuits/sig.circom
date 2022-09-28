9c62f7c520724754ca49eb8e2730aebeed214c5d
enricobottazzi <85900164+enricobottazzi@users.noreply.github.com>
2022-09-28 11:34:37 +0200

    removed overhead - fixed testing

--- circuits/sig.circom
+++
@@ -1,5 +0,0 @@
-pragma circom 2.0.2;
-
-include "../circom-ecdsa/circuits/ecdsa.circom";
-
-component main {public [r, s, msghash, pubkey]} = ECDSAVerifyNoPubkeyCheck(64, 4);
--- test/dvs.js
+++ test/dvs.js
@@ -46,7 +46,6 @@ describe("Designated Verifier Testing", function async() {
             var s = sig.slice(32, 64);
             var s_bigint = Uint8Array_to_bigint(s);
 
-            var priv_array = bigint_to_array(64, 4, BigInt(proverPrivKey));
             var r_array = bigint_to_array(64, 4, r_bigint);
             var s_array = bigint_to_array(64, 4, s_bigint);
             var msghash_array = bigint_to_array(64, 4, msghash_bigint);
@@ -74,7 +73,7 @@ describe("Designated Verifier Testing", function async() {
 
         it("should verify a valid proof (based on 2nd condition) coming from a dishonest verifier", async () => {
 
-            // let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
+            let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
             
             // message to be Signed
             let message = "Hello World!!!!";
@@ -88,42 +87,29 @@ describe("Designated Verifier Testing", function async() {
             var s = sig.slice(32, 64);
             var s_bigint = Uint8Array_to_bigint(s);
 
-            var priv_array = bigint_to_array(64, 4, BigInt(proverPrivKey));
+            // generate an invalid signature
             var r_array = bigint_to_array(64, 4, r_bigint + 1n);
             var s_array = bigint_to_array(64, 4, s_bigint);
             var msghash_array = bigint_to_array(64, 4, msghash_bigint);
             var pub0_array = bigint_to_array(64, 4, proverPubKey.x);
             var pub1_array = bigint_to_array(64, 4, proverPubKey.y);
 
-            let input = {"r": r_array,
+            // Generate Witness that doesn't satisfy the 1st condition and satisfies the 2nd condition condition (msg signature)
+            let witness = await circuit.calculateWitness({"r": r_array,
             "s": s_array,
             "msghash": msghash_array,
             "pubkey": [pub0_array, pub1_array],
             "privkey": bigintToTuple(BigInt(verifierPrivKey)),
             "addr": BigInt(verifierAddress)    
-            }
-
-            // input to json
-            let jsonInput = JSON.stringify(input);
-            // console.log("Input: ", jsonInput);
-            
-            // // Generate Witness that satisfies 1st condition (msg signature) and doesn't satisfy 2nd condition (priv key to address)
-            // let witness = await circuit.calculateWitness({"r": r_array,
-            //                                               "s": s_array,
-            //                                               "msghash": msghash_array,
-            //                                               "pubkey": [pub0_array, pub1_array],
-            //                                               "privkey": ,
-            //                                               "addr": verifierAddress,
-            //                                             });
-            
-            // // Evaluate witness to output 1 (namely true) 
-            // await circuit.assertOut(witness, {out: "1"})
-            // await circuit.checkConstraints(witness);
+            });    
+            // Evaluate witness to output 1 (namely true) 
+            await circuit.assertOut(witness, {out: "1"})
+            await circuit.checkConstraints(witness);
         });
 
         it("should verify a valid proof (based on both conditions)", async () => {
 
-            // let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
+            let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
             
             // message to be Signed
             let message = "Hello World!!!!";
@@ -137,44 +123,30 @@ describe("Designated Verifier Testing", function async() {
             var s = sig.slice(32, 64);
             var s_bigint = Uint8Array_to_bigint(s);
 
-            var priv_array = bigint_to_array(64, 4, BigInt(proverPrivKey));
             var r_array = bigint_to_array(64, 4, r_bigint);
             var s_array = bigint_to_array(64, 4, s_bigint);
             var msghash_array = bigint_to_array(64, 4, msghash_bigint);
             var pub0_array = bigint_to_array(64, 4, proverPubKey.x);
             var pub1_array = bigint_to_array(64, 4, proverPubKey.y);
 
-
-            let input = {"r": r_array,
-            "s": s_array,
-            "msghash": msghash_array,
-            "pubkey": [pub0_array, pub1_array],
-            "privkey": bigintToTuple(BigInt(verifierPrivKey)),
-            "addr": BigInt(verifierAddress)    
-            }
-
-            // input to json
-            let jsonInput = JSON.stringify(input);
-            console.log("Input: ", jsonInput);
+            // Generate Witness that satisfies 1st condition (msg signature) and satisfies 2nd condition (priv key to address)
+            let witness = await circuit.calculateWitness({"r": r_array,
+                                                          "s": s_array,
+                                                          "msghash": msghash_array,
+                                                          "pubkey": [pub0_array, pub1_array],
+                                                          "privkey": bigintToTuple(BigInt(verifierPrivKey)),
+                                                          "addr": verifierAddress,
+                                                        });
             
-            // // Generate Witness that satisfies 1st condition (msg signature) and doesn't satisfy 2nd condition (priv key to address)
-            // let witness = await circuit.calculateWitness({"r": r_array,
-            //                                               "s": s_array,
-            //                                               "msghash": msghash_array,
-            //                                               "pubkey": [pub0_array, pub1_array],
-            //                                               "privkey": ,
-            //                                               "addr": verifierAddress,
-            //                                             });
-            
-            // // Evaluate witness to output 1 (namely true) 
-            // await circuit.assertOut(witness, {out: "1"})
-            // await circuit.checkConstraints(witness);
+            // Evaluate witness to output 1 (namely true) 
+            await circuit.assertOut(witness, {out: "1"})
+            await circuit.checkConstraints(witness);
         });
 
 
         it("shouldn't verify an invalid proof (both conditions are not met) coming from the prover", async () => {
             
-            // let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
+            let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
             
             // message to be Signed
             let message = "Hello World";
@@ -189,84 +161,26 @@ describe("Designated Verifier Testing", function async() {
             var s = sig.slice(32, 64);
             var s_bigint = Uint8Array_to_bigint(s);
 
-            var priv_array = bigint_to_array(64, 4, BigInt(proverPrivKey));
+            // generate an invalid signature
             var r_array = bigint_to_array(64, 4, r_bigint + 1n);
             var s_array = bigint_to_array(64, 4, s_bigint);
-            var msghash_array = bigint_to_array(64, 4, msghash_bigint + BigInt(1));
+            var msghash_array = bigint_to_array(64, 4, msghash_bigint);
             var pub0_array = bigint_to_array(64, 4, proverPubKey.x);
             var pub1_array = bigint_to_array(64, 4, proverPubKey.y);
+                    
 
-
-            let input = {"r": r_array,
-            "s": s_array,
-            "msghash": msghash_array,
-            "pubkey": [pub0_array, pub1_array],
-            "privkey": bigintToTuple(BigInt(randomWalletPrivKey)),
-            "addr": BigInt(verifierAddress)    
-            }
-
-            // input to json
-            let jsonInput = JSON.stringify(input);
-            console.log("Input: ", jsonInput);
+            // Generate Witness that doesn't satisfies 1st condition (msg signature) and doesn't satisfy 2nd condition (priv key to address)
+            let witness = await circuit.calculateWitness({"r": r_array,
+                                                          "s": s_array,
+                                                          "msghash": msghash_array,
+                                                          "pubkey": [pub0_array, pub1_array],
+                                                          "privkey": bigintToTuple(BigInt(randPrivKey)),
+                                                          "addr": verifierAddress,
+                                                        });
             
-            
-
-            // // Generate Witness that doesn't satisfies 1st condition (msg signature) and doesn't satisfy 2nd condition (priv key to address)
-            // let witness = await circuit.calculateWitness({"r": r_array,
-            //                                               "s": s_array,
-            //                                               "msghash": msghash_array,
-            //                                               "pubkey": [pub0_array, pub1_array],
-            //                                               "privkey": bigintToTuple(BigInt(randPrivKey)),
-            //                                               "addr": verifierAddress,
-            //                                             });
-            
-            // // Evaluate witness to output 0 (namely false) 
-            // await circuit.assertOut(witness, {out: "0"})
-            // await circuit.checkConstraints(witness);
+            // Evaluate witness to output 0 (namely false) 
+            await circuit.assertOut(witness, {out: "0"})
+            await circuit.checkConstraints(witness);
         });
     });
-
-//     describe("Designated Verifier to Third Party Verifier", function () {
-    
-//         it("should verify a proof by the designated verifier which is invalid for 1st condition and valid for 2nd condition", async () => {
-            
-//             let circuit = await wasm_tester(path.join(__dirname, "../circuits", "dvs.circom"));
-            
-//             // false message that the Designated Verifier pretends that the prover signed;
-//             let message = "Fuck The World";
-//             // hash the message 
-//             let msghash_bigint = BigInt(ethers.utils.solidityKeccak256(["string"], [message]))
-//             let msghash = bigint_to_Uint8Array(msghash_bigint);     
-            
-//             // Designated Verifier signs the hashed message
-//             var sig = await sign(msghash, bigint_to_Uint8Array(BigInt(verifierPrivKey)), {canonical: true, der: false})            
-
-//             var r = sig.slice(0, 32);
-//             var r_bigint = Uint8Array_to_bigint(r);
-//             var s = sig.slice(32, 64);
-//             var s_bigint = Uint8Array_to_bigint(s);
-
-//             var priv_array = bigint_to_array(64, 4, BigInt(privKey));
-//             var r_array = bigint_to_array(64, 4, r_bigint);
-//             var s_array = bigint_to_array(64, 4, s_bigint);
-//             var msghash_array = bigint_to_array(64, 4, msghash_bigint);
-//             var pub0_array = bigint_to_array(64, 4, proverPubKey.x);
-//             var pub1_array = bigint_to_array(64, 4, proverPubKey.y);
-
-
-//             // Generate Witness that doesn't satisfy 1st condition (msg signature) and satisfy 2nd condition (priv key to address)
-//             let witness = await circuit.calculateWitness({"r": r_array,
-//                                                           "s": s_array,
-//                                                           "msghash": msghash_array,
-//                                                           "pubkey": [pub0_array, pub1_array],
-//                                                           "privkey": bigintToTuple(BigInt(verifierPrivKey)),
-//                                                           "addr": verifierAddress,
-//                                                         });
-            
-//             // Evaluate witness to output 1 (namely true) 
-//             await circuit.assertOut(witness, {out: "1"})
-//             await circuit.checkConstraints(witness);
-        
-//         });
-//     });
 });
